3d45e1a7a64690eb9add460605b1235b7e0362e6
Jan Schlieper <sleepy@mini.fritz.box>
2018-09-20 12:23:12 +0200

    updated comments

--- SBus.h
+++ SBus.h
@@ -1,5 +1,5 @@
 /*
-  Copyright 2018 macsleep
+  Copyright 2018 Jan Schlieper
 
   Permission to use, copy, modify, distribute, and sell this
   software and its documentation for any purpose is hereby granted
